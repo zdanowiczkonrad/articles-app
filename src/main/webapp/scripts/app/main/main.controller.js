f1aacd83a09ff5e1bc6996b447cb9155bb095f7d
zdanowiczkonrad <zdanowicz.konrad@gmail.com>
2015-02-09 00:09:15 +0100

    Added silly implementation

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,34 @@
 'use strict';
 
 angular.module('jhipsterApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Principal, Article, Comment) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+        $scope.articles = [];
+        $scope.comments = [];
+        $scope.newComment = {};
+        $scope.loadComments = function() {
+        	  Comment.query(function(result){
+	        	console.log(result)
+	        	$scope.comments = result;
+	        })
+        	};
+        $scope.loadData = function() {
+	        Article.query(function(result){
+	        	$scope.articles = result;
+	        	console.log(result);
+	        });
+		}
+		
+		$scope.loadData();
+		$scope.loadComments();
+		$scope.addComment = function(articleId) {
+			var newComment = new Comment({title:$scope.newComment.title,
+				content:$scope.newComment.content,date:new Date(),article:{id:articleId}});
+			newComment.$save(function(){$scope.loadComments()});
+		}
+
     });
